e1230ffc9720f48e0b0146467441ed4c01a0f1e4
Purity <puritysang180@gmail.com>
2025-02-25 09:45:21 +0300

    changed port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { authenticateToken, authorizeRoles } = require('./middleware/authmiddlewa
 const ProfileRoute = require('./routes/profileRoute')
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const app = express();const winston = require('winston');
 
 const logger = winston.createLogger({
