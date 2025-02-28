d1232b5f252860d3e15bfc086849768e29a42a2e
Purity-dev-614E <puritysang180@gmail.com>
2025-02-28 13:52:26 +0300

    added refresh refresh 1

--- utils/tokenservice.js
+++ utils/tokenservice.js
@@ -8,6 +8,14 @@ const generateRefreshToken = (user) => {
   return jwt.sign({ id: user.id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
 };
 
+let refreshTokens = [];  // In-memory storage (this should be replaced with a real database in production)
+
+const storeRefreshToken = (userId, refreshToken) => {
+    // Store the refresh token in your database or in-memory (this is just an example using an array)
+    refreshTokens.push({ userId, refreshToken });
+};
+
+module.exports = { storeRefreshToken };
 
 storeRefreshToken(user.id, generateAccessToken);
 
