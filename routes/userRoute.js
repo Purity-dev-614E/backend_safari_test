e8267010d7ce65b888aca732a22f2b399f42c156
Purity <puritysang180@gmail.com>
2025-03-01 15:09:32 +0300

    feat(user): add user profile routes for fetching and updating profiles

---
+++ routes/userRoute.js
@@ -0,0 +1,29 @@
+const User = require('../models/usermodel');
+const express = require('express');
+const router = express.Router();
+const { register, login, refreshToken } = require('../controllers/authcontroller');
+const auth = require('../middleware/authmiddleware');
+
+
+//fetch user profile
+router.get('/profile', auth, async (req, res) => {
+    try {
+        const user = await User.getById(req.user.id
+        );
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching user profile', error: error.message });
+    }
+}
+);
+
+//update user profile
+router.put('/profile', auth, async (req, res) => {
+    try {
+        const user = await User.updateById(req.user.id, req.body);
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating user profile', error: error.message });
+    }
+}
+);
--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const AuthRoute = require('./routes/authRoutes');
 const { authenticateToken, authorizeRoles } = require('./middleware/authmiddleware');
 const ProfileRoute = require('./routes/profileRoute')
 const eventsRoute = require('./routes/eventsRoute');
+const UserRoute = require('./routes/userRoute');
 require("dotenv").config();
 
 
 const PORT = process.env.PORT || 5001;
 const app = express();const winston = require('winston');
+const { verifyToken } = require('./utils/tokenservice');
 
 
 app.use(cors());
@@ -26,6 +28,7 @@ app.use('/api/attendance', AttendanceRoute, authenticateToken, authorizeRoles);
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use('/api/profile', ProfileRoute,authenticateToken);
 app.use('/api/events', eventsRoute, authenticateToken, authorizeRoles);
+app.use('api/user' ,UserRoute, authenticateToken, verifyToken);
 
 app.get('/safariapi', (req, res) => {
     res.json({ message: 'Welcome to the API' });
